a6c7d94ceb8d312814ab5d68316346db79a16341
Akinola24 <haykaychem@gmail.com>
2024-09-02 20:06:32 +0100

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,12 @@
+Python 3.2.5 (default, May 15 2013, 23:06:03) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> name = input('What is your name? ')
+What is your name? Akinola
+>>> age = input('What is your age? ')
+What is your age? 23
+>>> input('What is your location? ')
+What is your location? Nigeria
+'Nigeria'
+>>> print('Hello Akinola, you are 23 yeas of age and live in Nigeria.')
+Hello Akinola, you are 23 yeas of age and live in Nigeria.
+>>> 
